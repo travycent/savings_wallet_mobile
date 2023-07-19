21b93860145cb16f6b1051a3354b0111ea51d57f
Travy Cent <centtravy@gmail.com>
2023-07-19 23:21:27 +0300

    Added all components to Grid

--- lib/screens/banking/home_page.dart
+++ lib/screens/banking/home_page.dart
@@ -17,6 +17,10 @@ class HomePage extends StatefulWidget {
 }
 class _HomePageState extends State<HomePage> {
   final String? title = "Home";
+  final double gridHeightValue=60;
+  final double gridWidthValue=150;
+  double? activeWalletBalance = 1000000;
+  double? savingWalletBalance = 1000000;
   int _currentIndex = 0;
 
   @override
@@ -25,203 +29,120 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(title!),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-          child: Column(
-            children: [
-              Container(
-                height: 60,
-                margin: const EdgeInsets.all(2.0),
-                decoration:  BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(5.0),
+      body:SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 60,
+                  margin: const EdgeInsets.all(2.0),
+                  decoration:  BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5.0),
 
+                  ),
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          border: Border(right: BorderSide(color: Colors.black)),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              child: const Text(
+                                'Active Wallet',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              child:  Text(
+                                activeWalletBalance!.toString(),
+                                style: const TextStyle(fontSize: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          border: Border(bottom: BorderSide(color: Colors.black)),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              child: const Text(
+                                'Saving Wallet',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              child:  Text(
+                                savingWalletBalance!.toString(),
+                                style: const TextStyle(fontSize: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                child: GridView.count(
+                const SizedBox(height: 10.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const <Widget>[
+                      Text(
+                        'Target Balance',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '1,000,000',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                GridView.count(
                   crossAxisCount: 2,
                   shrinkWrap: true,
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        border: Border(right: BorderSide(color: Colors.black)),
-                      ),
-                      child: Column(
-                        children:  <Widget>[
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: const Text('Active Wallet'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: const Text('Investment Wallet'),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: const BoxDecoration(
-                        border: Border(bottom: BorderSide(color: Colors.black)),
-                      ),
-                      child: Column(
-                        children:  <Widget>[
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: const Text('Active Wallet'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: const Text('Investment Wallet'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 5.0),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children:  <Widget>[
-                    Container(
-                      // decoration: const BoxDecoration(
-                      //   border: Border(right: BorderSide(color: Colors.black)),
-                      // ),
-                      child: const Text('Target Balance'),
-                    ),
-                    Container(
-                      
-                      child: const Text('1,000,000'),
-                    ),
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: <Widget>[
+                    _buildGridItem('Deposit', const DepositPage()),
+                    _buildGridItem('Send', const SendPage()),
+                    _buildGridItem('Pay Bill', const PayBillPage()),
+                    _buildGridItem('Withdraw', const WithdrawPage()),
+                    _buildGridItem('Preferences', const SavingsPreferencesPage()),
+                    _buildGridItem('Target', const SavingsTargetPage()),
                     
                   ],
                 ),
-              ),
-              const SizedBox(height: 16.0),
-              GridView.count(
-                crossAxisCount: 2,
-                shrinkWrap: true,
-                children: <Widget>[
-                  Container(
-                    height: 100,
-                    width: 150,
-                    margin: const EdgeInsets.all(2.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Center(
-                        child:TextButton(
-                          child: const Text(
-                              'Deposit',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize:12.0,
-                              ),
-                            ),
-                          onPressed: (){
-                            //Move to the withdraw screen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder:(context) => const DepositPage()),
-                            );
-                          },
-                        ),
-                        
-                      ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 150,
-                    margin: const EdgeInsets.all(2.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Center(
-                        child:TextButton(
-                          child: const Text(
-                              'Send',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize:12.0,
-                              ),
-                            ),
-                          onPressed: (){
-                            //Move to the withdraw screen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder:(context) => const SendPage()),
-                            );
-                          },
-                        ),
-                        
-                      ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 150,
-                    margin: const EdgeInsets.all(2.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Center(
-                        child:TextButton(
-                          child: const Text(
-                              'Pay Bill',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize:12.0,
-                              ),
-                            ),
-                          onPressed: (){
-                            //Move to the withdraw screen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder:(context) => const PayBillPage()),
-                            );
-                          },
-                        ),
-                        
-                      ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 150,
-                    margin: const EdgeInsets.all(2.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Center(
-                        child:TextButton(
-                          child: const Text(
-                              'Withdraw',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize:12.0,
-                              ),
-                            ),
-                          onPressed: (){
-                            //Move to the withdraw screen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder:(context) => const WithdrawPage()),
-                            );
-                          },
-                        ),
-                        
-                      ),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -253,49 +174,26 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+  Widget _buildGridItem(String title, Widget page) {
+    return Container(
+      height: gridHeightValue,
+      width: gridWidthValue,
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+        },
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
 }
-// class _HomePageState extends State<HomePage> {
-//   final String? title="Home";
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title!)
-//       ),
-//       body: Center(
-//           child: Padding(
-//             padding:  const EdgeInsets.only(top:16.0, bottom : 16.0),
-//             child: Container(
-//               decoration: BoxDecoration(
-//                 border: Border.all(color: Colors.black),
-//                 borderRadius: BorderRadius.circular(8.0),
-//               ),
-//               child: Column(
-//                 children: [
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                     children: const<Widget>[
-//                       Text('Active Wallet'),
-//                       Text('Savings Wallet'),
-
-//                     ],
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                     children: const<Widget>[
-//                       Text('Investment Wallet'),
-//                       Text('Emergency Fund'),
-//                     ],
-//                   ),
-//                 ],   
-//               ),
-//             ),
-
-            
-//           ),
-        
-//       ),
-//     );
-//   }
-// }
 
